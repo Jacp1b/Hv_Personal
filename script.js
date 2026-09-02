7eeabbce6197d989be570173c1981f8ee611d57f
luisfeliciano01 <emelysg13@gmail.com>
2026-09-02 17:23:06 -0500

    V3

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function modoOsc() {
     document.getElementById('btnModoOsc').textContent = esOscuro ? '☀️ Modo Claro' : '🌙 Modo Oscuro';
 }
 function msjContac(){
-     alert("Gracias por contactarme");
+    alert("Gracias por contactarme");
 }
 
 function msjLkn(){
@@ -37,4 +37,23 @@ function msjLkn(){
 
 function msjProy(){
     alert ("Proyecto Disponible en GitHub");
+}
+
+
+function cambiarTexto() {
+    let texto = document.getElementById("textoSobreMi");
+    let contenidoActual = texto.textContent.trim();
+    let textoOriginal = "Tecnologo en Analisis y Desarrollo de Software (ADSO) con sóoida formación en el ciclo de vida del desarrollo de sistemas, levantamiento de requerimientos y maquetación web. Experiencia práctica en la creación de aplicaciones funcionales, modelado de bases de datos y desarrollo backend (Java, MySQL). Apasionado por la resolucion de problemas tecnicos, el trabajo bajo metodologías ágiles como Scrum y la entrega de codigo estructurado y mantenible.";
+    let textoSegundo = "A lo largo de mi formación académica y proyectos practicos, he enfocado mis esfuerzos en construir soluciones robustas y escalables. Me apasiona entender los requerimientos del negocio para traducirlos en bases de datos eficientes y lógica de backend sólida, asegurando siempre buenas prácticas de desarrollo y control de versiones.";
+    let textoTercero = "Ademas, me entusiasma seguir aprendiendo constantemente sobre nuevas tendencias tecnologicas, participar en equipos colaborativos y aportar valor real en proyectos donde la innovación, la adaptabilidad y el código limpio sean los verdaderos protagonistas. 🚀";
+
+    if (contenidoActual === textoOriginal) {
+        texto.textContent = textoSegundo;
+    } 
+    else if (contenidoActual === textoSegundo) {
+        texto.textContent = textoTercero;
+    } 
+    else {
+        texto.textContent = textoOriginal;
+    }
 }
